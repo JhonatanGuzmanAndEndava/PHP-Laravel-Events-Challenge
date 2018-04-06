6d7ca1dfe2cd8f29037ea1f668290d2f981a2de6
Jhonatan Guzman <jguzman@endava.com>
2018-04-06 18:59:53 -0500

    bug hotfix in events seed

--- EndavaEvents/database/factories/EventFactory.php
+++ EndavaEvents/database/factories/EventFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Event::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
-        'user_id' => random_int(0,50),
+        'user_id' => random_int(1,50),
         'category' => random_int(0,2) == 0? "Conferencia": random_int(0,1) == 0? "Seminario": "Curso",
         'place' => $faker->city,
         'address' => $faker->address,
