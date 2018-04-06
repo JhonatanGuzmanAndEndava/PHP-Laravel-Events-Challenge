d8235a888295889e9e961ceec2b04b2e352ffc95
Jhonatan Guzman <jguzman@endava.com>
2018-04-06 18:57:20 -0500

    Name and email fields in profile cannot be null

--- EndavaEvents/database/migrations/2018_04_03_185341_create_profiles_table.php
+++ EndavaEvents/database/migrations/2018_04_03_185341_create_profiles_table.php
@@ -16,9 +16,9 @@ class CreateProfilesTable extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
-            $table->string('name')->nullable();
+            $table->string('name');
             $table->string('lastname')->nullable();
-            $table->string('email')->nullable();
+            $table->string('email');
             $table->string('phone')->nullable();
             $table->string('cc')->nullable();
             $table->string('address')->nullable();
