037947dfa754b96df3d768377f0230ad633cae67
Jhonatan Guzman <jguzman@endava.com>
2018-04-06 18:50:42 -0500

    bug hotfix in profile seed

--- EndavaEvents/database/factories/ProfileFactory.php
+++ EndavaEvents/database/factories/ProfileFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Profile::class, function (Faker $faker) {
     return [
-        'user_id' => random_int(0,50),
+        'user_id' => random_int(1,50),
         'name' => $faker->name,
         'email' => $faker->email,
         'photo' => asset("storage/unknow.jpg"),
